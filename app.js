39dd56968bf1d25389d88fd24fa4e04e416d5887
natans <nutevsky@gmail.com>
2025-05-09 12:22:33 +0200

    v1.1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import path from "path";
-
+import { fileURLToPath } from "url";
 
 import dotenv from 'dotenv'
 dotenv.config();
@@ -13,9 +13,14 @@ import express from "express"
 const app = express();
 const port = process.env.PORT || 8080;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+//app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/query", async (req, res) => {
   //res.send("Hello, server is working!");
